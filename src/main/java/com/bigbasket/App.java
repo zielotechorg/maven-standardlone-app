ef62bdf6a94735015cd9835780ef1b0f9f9cc970
subhadarsinizielotech <116076399+subhadarsinizielotech@users.noreply.github.com>
2022-12-25 20:40:45 +0530

    Update App.java

--- src/main/java/com/bigbasket/App.java
+++ src/main/java/com/bigbasket/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "good morning.....everyone" );
+        System.out.println( "good morning.....to all" );
     }
 }
